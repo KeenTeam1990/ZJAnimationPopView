4f4e8d7424f7f030a4d8650e6711952552cf30e9
KeenTeam <812144991@qq.com>
2017-08-31 13:08:15 +0800

    ..

---
+++ README.md
@@ -0,0 +1,103 @@
+# ZJAnimationPopView
+
+[![Platform](https://img.shields.io/badge/platform-iOS-red.svg)](https://developer.apple.com/iphone/index.action)
+[![Language](http://img.shields.io/badge/language-OC-yellow.svg?style=flat )](https://en.wikipedia.org/wiki/Objective-C) 
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](http://mit-license.org)
+[![CocoaPods Compatible](https://img.shields.io/cocoapods/v/ZJAnimationPopView.svg)](https://img.shields.io/cocoapods/v/ZJAnimationPopView.svg)
+[![Weibo](https://img.shields.io/badge/%E5%BE%AE%E5%8D%9A-%40Abnerzj-f65a14.svg?logo=data%3aimage%2fpng%3bbase64%2ciVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAE7ElEQVR4Ae1Wf2iUdRh%252ftuk2CVk4WgRm273v%252fXidtpEUiekOloRrQkama5UR9EcxVKIiCGmjQIIhze1uZf4wxWmGlEhkwWAohDJNppVxI9zde3rdus77Me92t93z9HyP965733uXY1RQ%252bMGH9%252fv4ft%252fv5%252fN9ns%252f3e4M7%252bN8h1gfVHPs5zkddcImfgzEX7Iz0wUPwbyDeB69G%252bwBzweSkjTMcJxO7YTH8k0i44H4m%252bjrigi%252f4eYQFnOXnZIGgGzd3QwPMBeR0zvNbHDZ%252fna1BXbx4AcwStz6G%252b7gd%252faIKoiLcFjXeDzUwW9yoUx5QJeVDVXYEOEiLwJismPaVdsECJtvKpO%252fH3bCRDkBltj0ueI0FIAdxfAq3g9glE%252b30SfYkB4oQ5NqYw3ZiBiNuMXjgp4gbLJpHjmjvpsI9sARmwpjVqjDJFT2hXoBXsn8HJghuu%252bveyAdle6O9cJCJQhrhBToOZVwFJW9SF3SAGbx1ylomuGkkLM4dR4HxY319Of9%252fk9di3%252baX7O%252f56%252bXtwceWZHcc3QUyk01kq9ELq4ighMdjImej7gUjfJLyFC%252bW5KDbhuQ4wc89qmy%252fafI%252b6bNa1wCD23BGa8ULWn5O88Hn%252bp1bbM3iw6IdzzHnihwGBhP%252fnN3xbng8m7vAk21Bb4ERVYti9cn23%252bdOaGrSg5qAFi7328ID4qbkfJoDefwm5HrIHwzPtKB%252fWSMGNz2H4R2dbJx%252biu07wCZyY%252fidHfjr0xtJdSybySMvgQEsopzJzzO5GumFOhBQrcpbZj0OPrOJbp08RZl4nP4K0%252bPjFP1oD11%252feKXRIwN%252b2bGySEQnlHLMyybXahvvZrXhgh3wQo%252firVNfIWYySAzUkEqlMBaLUTqdRg2699OhEI5vedlYQfE8GZDle8AMqtW%252btfCDG85mSo%252bNYQFoaGgIW1tbsaqqCsvKyqi6uhrb29txdHQ0LyCPyUlRObOWXLxWW1sJRvCEofzk%252bgZMXb2qW7Czs5NKSkqQp%252baCcuNFixbhyMiIbj4xUpevCGKTi8v2ipmAeK5n4Xe7qBDHjh0TZLooLS2l8vLyfO50OknwGnF9VVORp9gP%252b8GIQoXJM2d1JW1sbMzueOHChbh%252b%252fXqsra3Frq4u6u7uzlekoqKC4vF4kScCTzxZfCpku9tMQDA3ITVyOb9AIpEQiyOTk8fjQYFIJII1NTXEPsgLqKyspImJCZ2ATDSKqrK8uAWy%252fVkTAfZDuQnxo5%252flBQinC7OtXr06m%252bdEcd%252fzHhDR3Nxc5IGou9%252fMhJ5RWa4oFmBxLOdJKQ4KrGsl4eIcOjo6aP78%252beR2u2lwcJBaWlp0fhC7Hx4epkJMDl8gdemDxv6nfFZlDcwEr%252fTnUQy9%252fgbxgUeGOPPIJtPtWCMXrcDTp0%252frPJP45lv0N6ww3gMpvpI3QxHMRWQrEdy4GVNXfkCBqakpGhgYwLa2NmxqasINGzZQT08PhsNhFCD%252bl%252faMYmjrduQb1XiVB7xWZS3MFmqd7RFe4FxWuW0pjre%252fiPFDh0mIybABMZnETCJB07%252bFcPLCRYx9sg%252bDbc8TzzX2PO2VHHt0t99sQQClqmRbp8rKl7xYIt9HqyJ%252blIgvK5GbBXKofNa7fbIswd%252bBXyyWKtWitHhlR5cq2Y%252bLP8H41Fzi3%252flLquT4nvNBzvdyvo13vILFl8F%252fDXfwB6rTSvg9dQ5kAAAAAElFTkSuQmCC&style=flat&logoWidth=14&label=)](http://weibo.com/ioszj)
+[![Jianshu](https://img.shields.io/badge/%E7%AE%80%E4%B9%A6-%40Abnerzj-e55844.svg?logo=data%3aimage%2fpng%3bbase64%2ciVBORw0KGgoAAAANSUhEUgAAAHAAAAA0CAMAAABl90wDAAAAjVBMVEUAAADrcFvqb1rrcVztclzuc17tc13rcFrrb1rtcl3qcFvrcVrrcFvscFvrcFrud2Tsb1rtcVvrcFzqb1vscFv0eWPrcFvscFvrb1rscFzrcFzscV3%2fiX3rb1rtcVzrb1rrcFrrcFv%2ffmLrcFrsc1%2frcFrrcFrrcFvwdF3scFvrcFvzdWPrcFrscFvqb1raTo%2bjAAAALnRSTlMA9MRXKS4bym480mbOhIIOhVOL7J4X2WvwWU02BKtD38CRCaMm%2bea2IHlzE5ZgwvLvugAAAo5JREFUeAHt1%2bu%2bcloUx%2fG%2fg2TRkkKWSEnnGvd%2feXsORYfUw9L2Yu%2fn%2b7Y%2b85eGOQsuPBJWRzwySQgc1CYT6%2bFPTGI6Hm2IfbcLehN2qFjYxAN7lV95r0WwXH2DG9mK2Ghwow9gTywZ3Zk2Dm4DEva4iumZDBwCqhA2DqJPwipDYUYVYmBBVbTmwSi5%2b1K1gCqYOFGVddo8CJOYg1y2JjYwzZiExMyFCImt%2b5PJjthowswtbm21exaxmfbgkDe%2bwKYJsT4A7ebjRfsVCYEGwcqDqBBTLc6wGL89XBHbgfnlrtvOiY2nYLu8neLZqGZQLW7EMTFJQe5IQgJhSSzJkNNe3p77mkHoxDIQ%2b9JwdiJmF0HXxlkk5VPGs61UMzgk9s3BoP%2bjC5sUUGVhhHMwCVFaWpYla6iQqncsYppXii%2fBkJgM8ncHpD4JLkpcOEUotTja3EvwILEdDLCwmJGhXEyUatlvg1XvSyJ%2b%2bb3%2bh4LOZbVBV8Fofb5Es6sgFsQUT5de%2bmzQIObiNeNjwelMML5837dmnQT160GOroPedxXz3wuqVMV6FXR%2bCkrBJ7YzSzKxhZKLWgZ9ashuGZx%2fKOgOS%2frb4PJDwSlKi7dBzZceUU560j7Y8NlC%2fn8H3UFJ7yD47G%2bwdXA5LHUzQwOlxX8%2beCRB7nDjWyTEHwj6xNLqoKJeGC4xs33QIDau94u%2ftlsEQ0U4jSRiy3rBEC2COl1Jap2gP0ObYExXJp6C2fieH%2fdTXMx%2bFRxRYf3T8H%2fpnpjTMOi589zRtFE3GKW2ffCcgNi2frBajeCermJ0EHSoJG0%2fFtzgpSkVghlqBYPoT0FpnuGlyKLc%2bNhDjWDgx1O8pak23lM1wUMdvRRv%2fQNommOfb6EQtgAAAABJRU5ErkJggg%3d%3d&style=flat&logoWidth=24&label=)](http://www.jianshu.com/p/02c47821d218)
+
+
+- A fast, convenient view to show animation pop view, it support 72 kinds of animation pop effect.【一个快速便捷、无侵入、可扩展的动画弹框库，两句代码即可实现想要的动画弹框。支持手写和xib，支持横竖屏。可以配置出72种不同的弹框动画效果。】
+- PS:Recorded and found that the resulting loss of animation is not consistent, it is recommended to download and install Demo view.【录制后发现失帧造成动画效果不连贯，手机上浏览横竖屏切换的Gif图也被拉伸了，建议下载安装Demo查看。】
+---
+
+<div align=left>
+<img width="284.5" height="505.5" src="https://github.com/Abnerzj/Resources/blob/master/ZJImages/ZJAnimationPopView/ZJAnimationPopView_01.gif"/>
+<img width="284" height="506.5" src="https://github.com/Abnerzj/Resources/blob/master/ZJImages/ZJAnimationPopView/ZJAnimationPopView_02.gif"/>
+<img width="284.5" height="505.5" src="https://github.com/Abnerzj/Resources/blob/master/ZJImages/ZJAnimationPopView/ZJAnimationPopView_03.gif"/>
+</div>
+<div align=left>
+<img width="284.5" height="505" src="https://github.com/Abnerzj/Resources/blob/master/ZJImages/ZJAnimationPopView/ZJAnimationPopView_04.gif"/>
+<img width="285" height="506.5" src="https://github.com/Abnerzj/Resources/blob/master/ZJImages/ZJAnimationPopView/ZJAnimationPopView_05.gif"/>
+</div>
+<div align=left>
+<img width="696" height="494" src="https://github.com/Abnerzj/Resources/blob/master/ZJImages/ZJAnimationPopView/ZJAnimationPopView_06.gif"/>
+</div>
+
+---
+
+# Getting Started【开始使用】
+## Features【能做什么】
+- 动画样式分为**显示时**和**移除时**两种类型，两两组合可以配置出72种**(8*9)**不同的弹框动画效果。
+- 动画样式可扩展，代码**简洁易读**，自行添加想要的显示样式和移除样式即可。
+- 自定义控件支持**手写**和**xib**。
+- 支持**横竖屏**和**横竖屏切换监听**。
+- 支持自定义**背景透明度**。
+- 支持设置显示时和移除时的**动画时长**。
+
+## Installation【安装】
+
+### From CocoaPods【使用CocoaPods】
+
+```ruby
+pod 'ZJAnimationPopView'
+```
+
+### Manually【手动导入】
+- Drag all source files under floder `ZJAnimationPopView` to your project.【将`ZJAnimationPopView`文件夹中的所有源代码拽入项目中】
+- Import the main header file：`#import "ZJAnimationPopView.h"`【导入主头文件：`#import "ZJAnimationPopView.h"`】
+
+```objc
+ZJAnimationPopView.h                      
+ZJAnimationPopView.m
+```
+# Examples【示例】
+## Direct Use【直接使用】
+```objc
+// 1.初始化
+ZJAnimationPopView *popView = [[ZJAnimationPopView alloc] initWithCustomView:[[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)] popStyle:ZJAnimationPopStyleScale dismissStyle:ZJAnimationDismissStyleScale];
+// 2.显示弹框
+[popView pop];
+```
+## User-defined properties【自定义想要的弹框效果】
+```objc
+// 1.Get custom view【获取自定义控件】
+UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+
+UIImageView *imageView = [[UIImageView alloc] initWithFrame:customView.bounds];
+imageView.image = [UIImage imageNamed:@"personal_info_02"];
+[customView addSubview:imageView];
+
+// 2.Init【初始化】
+ZJAnimationPopView *popView = [[ZJAnimationPopView alloc] initWithCustomView:customView popStyle:ZJAnimationPopStyleScale dismissStyle:ZJAnimationDismissStyleScale];
+
+// 3.Set properties,can not be set【设置属性，可不设置使用默认值，见注解】
+// 3.1 显示时点击背景是否移除弹框
+popView.isClickBGDismiss = ![_customView isKindOfClass:[SlideSelectCardView class]];
+// 3.2 显示时背景的透明度
+popView.popBGAlpha = 0.5f;
+// 3.3 显示时是否监听屏幕旋转
+popView.isObserverOrientationChange = YES;
+// 3.4 显示时动画时长
+popView.popAnimationDuration = 0.8f;
+// 3.5 移除时动画时长
+popView.dismissAnimationDuration = 0.8f;
+
+// 3.6 显示完成回调
+popView.popComplete = ^{
+NSLog(@"显示完成");
+};
+// 3.7 移除完成回调
+popView.dismissComplete = ^{
+NSLog(@"移除完成");
+};
+
+// 4.pop view【显示弹框】
+[popView pop];
+```
+
+## 期待
+* 如果在使用过程中遇到BUG，请Issues我，谢谢。
+* 这个库支持扩展动画样式，如果你想为ZJAnimationPopView输出代码，请拼命Pull Requests我，让更多的老铁们都能使用到你那吊炸天的动画效果。
+* 联系我😯 ：[GitHub](https://github.com/KeenTeam1990/ZJAnimationPopView.git)
---
+++ ZJAnimationPopViewDemo/ViewController.m
@@ -0,0 +1,232 @@
+//
+//  ViewController.m
+//  ZJAnimationPopViewDemo
+//
+//  Created by Abnerzj on 2017/8/12.
+//  Copyright © 2017年 Abnerzj. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZJAnimationPopView.h"
+#import "PersonalInfoPopView.h"
+#import "ZJAlertView.h"
+#import "SlideSelectCardView.h"
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+{
+    UITableView *_tableView;
+    NSArray *_dataList;
+    UISegmentedControl *_segCtrl;
+    id _customView;
+    PersonalInfoType _personalInfoType;
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self initDataAndSubViews];
+}
+
+#pragma mark - UITableView DataSource & Delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _dataList.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ID = @"ZJAnimationPopViewCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    
+    if (indexPath.row >= 0 && indexPath.row < _dataList.count) {
+        NSDictionary *dict = _dataList[indexPath.row];
+        cell.textLabel.text = dict[@"title"];
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row >= 0 && indexPath.row < _dataList.count) {
+        NSDictionary *dict = _dataList[indexPath.row];
+        NSInteger style = ((NSNumber *)dict[@"style"]).integerValue;
+        [self showPopAnimationWithAnimationStyle:style];
+    }
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 120.0f)];
+    UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, bgView.frame.size.width - 30, 80.0f)];
+    headerLabel.text = @"ZJAnimationPopView一个快速便捷、无侵入、可扩展的动画弹框库，两句代码即可实现想要的动画弹框。支持手写和xib，支持横竖屏。动画样式分为显示时和移除时两种类型，两两组合可以配置出72种(8*9)不同的弹框动画效果。";
+    headerLabel.textColor = [UIColor darkTextColor];
+    headerLabel.numberOfLines = 0;
+    headerLabel.font = [UIFont systemFontOfSize:13.0f];
+    headerLabel.textAlignment = NSTextAlignmentLeft;
+    [bgView addSubview:headerLabel];
+    
+    if (!_segCtrl) {
+        _segCtrl = [[UISegmentedControl alloc] initWithItems:@[@"滑动选择卡片", @"个人资料卡片", @"Alert弹框"]];
+        _segCtrl.selectedSegmentIndex = 0;
+        _segCtrl.apportionsSegmentWidthsByContent = YES;
+        [_segCtrl addTarget:self action:@selector(segCtrlAction:) forControlEvents:UIControlEventValueChanged];
+        [self segCtrlAction:_segCtrl];
+    }
+    _segCtrl.frame = CGRectMake(15, CGRectGetMaxY(headerLabel.frame), bgView.frame.size.width - 30 , 32.0f);
+    [bgView addSubview:_segCtrl];
+
+    return bgView;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 120.0f;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44.0f;
+}
+
+#pragma mark 显示弹框
+- (void)showPopAnimationWithAnimationStyle:(NSInteger)style
+{
+    ZJAnimationPopStyle popStyle = (style == 8) ? ZJAnimationPopStyleCardDropFromLeft : (ZJAnimationPopStyle)style;
+    ZJAnimationDismissStyle dismissStyle = (ZJAnimationDismissStyle)style;
+    
+    // 1.初始化
+    ZJAnimationPopView *popView = [[ZJAnimationPopView alloc] initWithCustomView:_customView popStyle:popStyle dismissStyle:dismissStyle];
+    
+    // 2.设置属性，可不设置使用默认值，见注解
+    // 2.1 显示时点击背景是否移除弹框
+    popView.isClickBGDismiss = ![_customView isKindOfClass:[SlideSelectCardView class]];
+    // 2.2 显示时背景的透明度
+    popView.popBGAlpha = 0.5f;
+    // 2.3 显示时是否监听屏幕旋转
+    popView.isObserverOrientationChange = YES;
+    // 2.4 显示时动画时长
+//    popView.popAnimationDuration = 0.8f;
+    // 2.5 移除时动画时长
+//    popView.dismissAnimationDuration = 0.8f;
+
+    // 2.6 显示完成回调
+    popView.popComplete = ^{
+        NSLog(@"显示完成");
+    };
+    // 2.7 移除完成回调
+    popView.dismissComplete = ^{
+        NSLog(@"移除完成");
+    };
+    
+    // 3.处理自定义视图操作事件
+    [self handleCustomActionEnvent:popView];
+    
+    // 4.显示弹框
+    [popView pop];
+}
+
+#pragma mark 选项卡监听
+- (void)segCtrlAction:(UISegmentedControl *)segCtrl
+{
+    if (segCtrl.selectedSegmentIndex == 0) {
+//        // 手写控件
+//        UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+//        
+//        UIImageView *imageView = [[UIImageView alloc] initWithFrame:customView.bounds];
+//        imageView.image = [UIImage imageNamed:@"personal_info_02"];
+//        [customView addSubview:imageView];
+//        _customView = customView;
+        
+        // xib
+        _customView = [SlideSelectCardView xib];
+        if (_personalInfoType != PersonalInfoTypeDouYu) {
+            [_customView updatePage:(_personalInfoType - 1)];
+        }
+    } else if (segCtrl.selectedSegmentIndex == 1) {
+        _customView = [PersonalInfoPopView xibWithPersonalInfoType:_personalInfoType]; 
+    } else if (segCtrl.selectedSegmentIndex == 2) {
+        _customView = [ZJAlertView xib];
+    }
+}
+
+#pragma mark 处理自定义视图操作事件
+- (void)handleCustomActionEnvent:(ZJAnimationPopView *)popView
+{
+    // 在监听自定义视图的block操作事件时，要使用弱对象来避免循环引用
+    __weak typeof(popView) weakPopView = popView;
+    __weak typeof(_segCtrl) weakSegCtrl = _segCtrl;
+    if ([_customView isKindOfClass:[SlideSelectCardView class]]) {
+        SlideSelectCardView *cardView = _customView;
+        cardView.selectActionBlock = ^(NSUInteger currentPage) {
+            _personalInfoType = (PersonalInfoType)(currentPage + 1);
+            NSString *segTitle = (_personalInfoType == PersonalInfoTypeDouYu) ? @"斗鱼" : ((_personalInfoType == PersonalInfoTypeYY) ? @"YY" : @"映客");
+            [weakSegCtrl setTitle:[NSString stringWithFormat:@"%@个人资料卡片", segTitle] forSegmentAtIndex:1];
+            weakSegCtrl.apportionsSegmentWidthsByContent = YES;
+            
+            [weakPopView dismiss];
+        };
+    } else if ([_customView isKindOfClass:[PersonalInfoPopView class]]) {
+        PersonalInfoPopView *infoPopView = _customView;
+        infoPopView.closeActionBlock = ^{
+            [weakPopView dismiss];
+        };
+    } else if ([_customView isKindOfClass:[ZJAlertView class]]) {
+        ZJAlertView *alertView = _customView;
+        alertView.canceSureActionBlock = ^(BOOL isSure) {
+            [weakPopView dismiss];
+            NSLog(@"点击了%@", isSure ? @"确定" : @"取消");
+        };
+    }
+}
+
+#pragma mark 设备方向改变
+- (void)statusBarOrientationChange:(NSNotification *)notification
+{
+    _tableView.frame = self.view.bounds;
+    [_tableView reloadData];
+}
+
+- (void)initDataAndSubViews
+{
+    _personalInfoType = PersonalInfoTypeDouYu;
+    _dataList = @[@{@"title" : @"卡片式掉落动画(从左侧)", @"style" : @6},
+                  @{@"title" : @"卡片式掉落动画(从右侧)", @"style" : @7},
+                  @{@"title" : @"卡片式掉落动画(往顶部平滑消失)", @"style" : @8},
+                  @{@"title" : @"从顶部掉落晃动动画", @"style" : @2},
+                  @{@"title" : @"从底部掉落晃动动画", @"style" : @3},
+                  @{@"title" : @"从左侧掉落晃动动画", @"style" : @4},
+                  @{@"title" : @"从右侧掉落晃动动画", @"style" : @5},
+                  @{@"title" : @"缩放动画", @"style" : @1},
+                  @{@"title" : @"无动画", @"style" : @0}];
+    
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    _tableView.separatorInset = UIEdgeInsetsZero;
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    [self.view addSubview:_tableView];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(statusBarOrientationChange:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
